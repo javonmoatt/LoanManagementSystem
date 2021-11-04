038209b54344f260bffb3e7e339a269750876895
Javon Moatt <javonmoatt@gmail.com>
2021-11-03 21:44:58 -0500

    update files

--- src/app/loans/loans.module.ts
+++ src/app/loans/loans.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { LoansRoutingModule } from './loans-routing.module';
 import { LoansComponent } from './loans.component';
+import { ViewLoansComponent } from './view-loans/view-loans.component';
+import { AddLoansComponent } from './add-loans/add-loans.component';
+import { EditLoansComponent } from './edit-loans/edit-loans.component';
+import { ListLoansComponent } from './list-loans/list-loans.component';
+import { DeleteLoansComponent } from './delete-loans/delete-loans.component';
 
 
 @NgModule({
   declarations: [
-    LoansComponent
+    LoansComponent,
+    ViewLoansComponent,
+    AddLoansComponent,
+    EditLoansComponent,
+    ListLoansComponent,
+    DeleteLoansComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/loans/view-loans/view-loans.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-loans',
+  templateUrl: './view-loans.component.html',
+  styleUrls: ['./view-loans.component.css']
+})
+export class ViewLoansComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
