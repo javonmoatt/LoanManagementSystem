985316a48a2ef6d1905ece0f8f12745bc6906cc0
javonmoatt <46875915+javonmoatt@users.noreply.github.com>
2025-02-10 12:25:06 -0500

    Delete src directory

--- src/app/interface/customer.ts
+++
@@ -1,9 +0,0 @@
-export interface ICustomer {
-  id: string,
-  firstName: string,
-  lastName: string,
-  emailAddress: string,
-  phoneNumber: string,
-  dob: string,
-  department: string,
-}
--- src/app/models/loan-categories.ts
+++
@@ -1,7 +0,0 @@
-export enum LoanCategory{
-  CONSUMER = 'consumer',
-  COMMERCIAL = 'commercial',
-  CREDIT_LINE = 'credit-line',
-  MORTAGE = 'mortage',
-  AUTO = 'auto'
-}
